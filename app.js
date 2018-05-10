744b3997738e472b1ee34269880701aca31d1005
Mike Dettmer <mike@mikedettmer.com>
2018-05-10 16:22:59 -0400

    adds login form and auth checking middleware

--- app.js
+++ app.js
@@ -3,24 +3,19 @@ var exphbs  = require('express-handlebars');
 const path = require('path');
 const session = require('express-session');
 const sassMiddleware = require('node-sass-middleware');
+const bodyParser = require('body-parser');
 var app = express();
 
 app.engine('.hbs', exphbs({extname: '.hbs', defaultLayout: 'main'}));
 app.set('view engine', '.hbs');
-
-app.get('/', function (req, res) {
-    res.redirect('/dm');
-});
-
-app.get('/player/:name', (req, res) => {
-    res.render('home', {user: req.params.name, type: 'player'});
-});
-
-app.get('/dm', (req, res) => {
-    res.render('home', {user: 'dm'});
-});
-
 app.use(session({ secret: 'keyboard cat', saveUninitialized: true, resave: false}));
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
 app.use(sassMiddleware({
     src: path.join(__dirname, 'public'),
     dest: path.join(__dirname, 'public'),
@@ -30,4 +25,45 @@ app.use(sassMiddleware({
 }));
 
 app.use(express.static(path.join(__dirname, 'public')));
+
+
+let requireLogin = (req, res, next) => {
+    if (req.session.isAuth) {
+        next();
+    } else {
+        res.redirect('login');
+    }
+}
+
+
+
+app.get('/', function (req, res) {
+    res.redirect('/dm');
+});
+
+app.get('/player/:name', (req, res) => {
+    res.render('home', {user: req.params.name, type: 'player'});
+});
+
+
+app.get('/login', (req, res) => {
+    res.render('login');
+});
+
+app.post('/login', (req, res) => {
+    if (req.body.username == 'mike' && req.body.password == 'pass') {
+        req.session.isAuth = true;
+        res.redirect('/dm');
+    } else {
+        res.redirect('/login');
+    }
+});
+
+app.all('/dm/*', requireLogin, (req, res, next) => next());
+
+app.get('/dm', requireLogin, (req, res) => {
+    res.render('home', {user: 'dm'});
+});
+
+
 app.listen(3000);
